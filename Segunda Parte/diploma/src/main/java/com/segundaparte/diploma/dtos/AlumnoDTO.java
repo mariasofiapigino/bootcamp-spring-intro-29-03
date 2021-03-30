b817324c115b0f1b33d75ab472a91c0d8c844c28
Maria Sofia Pigino <sofia.pigino@mercadolibre.com>
2021-03-30 10:24:50 -0300

    Methods moved to service

--- Segunda Parte/diploma/src/main/java/com/segundaparte/diploma/dtos/AlumnoDTO.java
+++ Segunda Parte/diploma/src/main/java/com/segundaparte/diploma/dtos/AlumnoDTO.java
@@ -8,12 +8,4 @@ import java.util.List;
 public class AlumnoDTO {
     private String nombre;
     private List<MateriaDTO> materias;
-
-    public Double calcularPromedio(){
-        Double total = 0.0;
-        for (MateriaDTO materia:materias) {
-            total += materia.getNota();
-        }
-        return total/materias.size();
-    }
 }
--- Segunda Parte/diploma/src/main/java/com/segundaparte/diploma/services/AlumnoService.java
+++ Segunda Parte/diploma/src/main/java/com/segundaparte/diploma/services/AlumnoService.java
@@ -1,16 +1,25 @@
 package com.segundaparte.diploma.services;
 
 import com.segundaparte.diploma.dtos.AlumnoDTO;
+import com.segundaparte.diploma.dtos.MateriaDTO;
 import com.segundaparte.diploma.dtos.MensajeDTO;
 
 public class AlumnoService {
 
     public static MensajeDTO calcularPromedio(AlumnoDTO alumno){
-        Double promedio = alumno.calcularPromedio();
+        Double promedio = calcularPromedioAlumno(alumno);
         String mensaje = "Ha aprobado";
         if (promedio < 4) mensaje = "No aprobo";
         if (promedio > 9) mensaje = "Felicitaciones! Ha aprobado con sobresaliente";
 
         return new MensajeDTO(mensaje, promedio, alumno);
+    }
+
+    public static Double calcularPromedioAlumno(AlumnoDTO alumno){
+        Double total = 0.0;
+        for (MateriaDTO materia:alumno.getMaterias()) {
+            total += materia.getNota();
+        }
+        return total/alumno.getMaterias().size();
     }
 }
